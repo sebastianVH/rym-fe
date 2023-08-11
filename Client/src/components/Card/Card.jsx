b6843bfb3135a7e78597db67b513226c0ae05bfe
sebastianVH <sebastian.yacono@gmail.com>
2023-08-11 15:46:39 -0300

    Client Side

---
+++ Client/src/components/Card/Card.jsx
@@ -0,0 +1,56 @@
+
+import React, { useState, useEffect } from "react";
+import styles from './Card.module.css'
+import { Link } from "react-router-dom";
+import { addFav,removeFav } from "../../redux/actions";
+import { connect } from "react-redux";
+
+
+function Card({character, close, addFav, removeFav, myFavorites}) {
+
+
+   const [isFav, setIsFav] = useState(false)
+
+   useEffect(() => {
+      myFavorites.forEach((fav) => {
+         if (fav.id === character.id) {
+            setIsFav(true);
+         }
+      });
+   }, [myFavorites,character.id]);  
+
+   const handleFavorite = () => {
+      (isFav) ? removeFav(character.id): addFav(character)
+      setIsFav(!isFav)
+   }
+
+   return (
+      <div className={styles.cardContainer} key={character.id} transition-style={"in:square:center"}>
+         {isFav ? (<button title={"Quitar de Fav"} className={styles.btnFav} onClick={handleFavorite}>❤️</button>) : (<button title={"Agregar a Fav"} className={styles.btnFav} onClick={handleFavorite}>🤍</button>)}
+         <button className={styles.buttonClose} onClick={() => {close(character.id);removeFav(character.id)}}>X</button>
+         <Link className={styles.links} to={`/detail/${character.id}`}>
+            <h1 className={styles.textDetail}>{character.name}</h1>
+         </Link>
+         <h3 className={styles.textDetail}>{character.status}</h3>
+         <h3 className={styles.textDetail}>{character.species}</h3>
+         <h3 className={styles.textDetail}>{character.gender}</h3>
+         <h3 className={styles.textDetail}>{character.origin.name}</h3>
+         <img className={styles.imgDetail} src={character.image} alt='' />
+      </div>
+   );
+}
+
+export const mapDispatchToProps = (dispatch) => {
+   return {
+      addFav: (character) => dispatch(addFav(character)),
+      removeFav: (id) => dispatch(removeFav(id))
+   }
+}
+
+export function mapStateToProps(state) {
+   return{
+      myFavorites: state.myFavorites
+   }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Card)
---
+++ Client/src/redux/actions.js
@@ -0,0 +1,93 @@
+import axios from 'axios'
+
+export const ADD_FAV = 'ADD_FAV'
+export const REMOVE_FAV = 'REMOVE_FAV'
+export const FILTER = "FILTER"
+export const ORDER = "ORDER"
+
+// export const addFav = (character) => {
+//     return {
+//         type: ADD_FAV,
+//         payload: character
+//     }
+// }
+
+export const addFav = (character) => {
+    const endpoint = 'http://localhost:3001/rickandmorty/fav';
+    return async (dispatch) => {
+        try {
+            const {data} = await axios.post(endpoint, character)
+            return dispatch({
+               type: 'ADD_FAV',
+               payload: data,
+            });
+        } catch (error) {
+            return error.message
+        }
+
+    };
+ };
+
+
+// export const addFav = (character) => {
+//     const endpoint = 'http://localhost:3001/rickandmorty/fav';
+//     return (dispatch) => {
+//        axios.post(endpoint, character).then(({ data }) => {
+//         console.log(data);
+//           return dispatch({
+//              type: 'ADD_FAV',
+//              payload: data,
+//           });
+//        });
+//     };
+//  };
+
+// export const removeFav = (id) => {
+//     return {
+//         type: REMOVE_FAV,
+//         payload: id
+//     }
+// }
+
+
+// export const removeFav = (id) => {
+//     const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
+//     return (dispatch) => {
+//        axios.delete(endpoint).then(({ data }) => {
+//           return dispatch({
+//              type: 'REMOVE_FAV',
+//              payload: data,
+//        });
+//        });
+//     };
+// };
+
+export const removeFav = (id) => {
+    const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
+    return async (dispatch) => {
+        try {
+            const {data} = await axios.delete(endpoint)
+            return dispatch({
+               type: 'REMOVE_FAV',
+               payload: data,
+         });
+        } catch (error) {
+            return error.message
+        }
+    };
+};
+
+
+export const filterCards = (gender) => {
+    return {
+        type: FILTER,
+        payload: gender
+    }
+}
+
+export const orderCards = (order) => {
+    return {
+        type: ORDER,
+        payload: order
+    }
+}
