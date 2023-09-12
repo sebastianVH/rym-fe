0fe54a8c93315babc06da85ca18a368f945d8e03
sebastianVH <sebastian.yacono@gmail.com>
2023-09-12 17:54:44 -0300

    change

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {store } from './redux/store'
-import axios from 'axios/lib/axios'
+import axios from 'axios'
 
 axios.defaults.baseURL = 'https://rym-be-production.up.railway.app'
 
